c76389ae0250e93fd7bc5e91a4d1375f49a1795f
thanhmai <duongthanhmai999@gmail.com>
2026-03-05 01:08:09 +0700

    add new image

--- vite-project/src/Component/Artist/Artist.jsx
+++ vite-project/src/Component/Artist/Artist.jsx
@@ -4,20 +4,20 @@ import { useNavigate } from 'react-router-dom';
 import EminemImg from '../ImageArtist/Eminem.png';
 import NavBar from '../Albums/NavBar';
 import ArtistPopularTable from './ArtistPopularTable';
-import Wedontcare from '../ImageArtist/Wedontcare.png';
-import Superman from '../ImageArtist/Superman.png';
-import RollingInTheDeep from '../ImageArtist/RollingInTheDeep.png';
-import AnotherLove from '../ImageArtist/AnotherLove.png';
-import Greedy from '../ImageArtist/Greedy.png';
+import WithoutMe from '../ImageArtist/WithoutMe.png';
+import mockingbird from '../ImageArtist/mockingbird.png';
+import TheRealSlimSha from '../ImageArtist/TheRealSlimSha...png';
+import LoseYourself from '../ImageArtist/LoseYourself.png';
+import Godzila from '../ImageArtist/Godzila.png';
 
 const Artist = () => {
     const navigate = useNavigate();
     const popularSongs = [
-        { image: Wedontcare, title: 'Without Me', artist: 'Eminem', release: 'May 15, 2002', played: '21,215,618', duration: '4:50' },
-        { image: Superman, title: 'mockingbird', artist: 'Eminem', release: 'Apr 25, 2005', played: '19,856,112', duration: '4:10' },
-        { image: RollingInTheDeep, title: 'The Real Slim Sha..', artist: 'Eminem', release: 'May 23, 2000', played: '18,234,567', duration: '4:44' },
-        { image: AnotherLove, title: 'Lose Yourself', artist: 'Eminem', release: 'Oct 28, 2002', played: '16,543,210', duration: '5:26' },
-        { image: Greedy, title: 'Godzilla', artist: 'Eminem', release: 'Nov 30, 2023', played: '15,123,456', duration: '3:30' },
+        { image: WithoutMe, title: 'Without Me', artist: 'Eminem', release: 'May 15, 2002', played: '21,215,618', duration: '4:50' },
+        { image: mockingbird, title: 'mockingbird', artist: 'Eminem', release: 'Apr 25, 2005', played: '19,856,112', duration: '4:10' },
+        { image: TheRealSlimSha, title: 'The Real Slim Sha..', artist: 'Eminem', release: 'May 23, 2000', played: '18,234,567', duration: '4:44' },
+        { image: LoseYourself, title: 'Lose Yourself', artist: 'Eminem', release: 'Oct 28, 2002', played: '16,543,210', duration: '5:26' },
+        { image: Godzila, title: 'Godzilla', artist: 'Eminem', release: 'Nov 30, 2023', played: '15,123,456', duration: '3:30' },
     ];
 
     return (
@@ -31,6 +31,9 @@ const Artist = () => {
                         <NavBar onBack={() => navigate(-1)} />
                     </div>
                     <ArtistPopularTable songs={popularSongs} />
+                    <h2 className="artist-albums-title">
+                        Artist's <span className="artist-albums-accent">Albums</span>
+                    </h2>
                 </div>
             </div>
         </div>
